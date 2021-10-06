baaa64be8ab921f017863041189e4793feaf2838
Andrien Wiandyano <arnnied03@gmail.com>
2021-10-06 12:08:44 +0700

    refactor: renamed `area` module to `sector`

--- dungeonrun/sector/base.py
+++ dungeonrun/sector/base.py
@@ -2,55 +2,52 @@ from importlib import import_module
 from time import sleep
 
 from dungeonrun.utils import convert_to_readable, convert_to_keys, import_from_pack
-from dungeonrun.area import exceptions as exc
 
-class BaseArea:
+class BaseSector:
     """
-    Class to inherit when creating an area.
-    
-    Note that this class should be inherited last (rightmost) after any area mixin used.
+    Class to inherit when creating an sector.
+
+    Note that this class should be inherited last (rightmost) after any sector mixin used.
     """
 
     player = None
-    paths = None # {"area_key": "area_module_location"}
-    next_area = None
+
+    paths = None # {"sector_key": "sector_module_location"}
     path_separator = " :: "
+
+    next_sector = None
 
     def __init__(self):
         super().__init__()
 
-    # def attribute_check(self):
-    #     if type(self.paths) is not str or type(self.paths) is not dict or type(self.paths) is not None:
-    #         pass # Raise paths improper
-    #     if type(self.path_separator) is not str:
-    #         pass 
-    
     def execute(self):
         """Main function to call"""
 
         self.paths_check()
-        while self.next_area == None:
+        while self.next_sector == None:
             self.display_available_path()
-            self.set_next_area()
+            self.set_next_sector()
 
-        imported_area = self.import_next_area(self.next_area)
+        imported_sector = self.import_next_sector(self.next_sector)
 
-        return imported_area
+        return imported_sector
 
     def paths_check(self):
-        """Check if self.paths is a string pointing to an area
-        OR if self.paths is None then it will be considered an 'ending'"""
+        """
+        Check if self.paths is a string pointing to an sector
+        OR if self.paths is None then it will be considered an 'ending'
+        """
 
-        if self.paths == 'end':
-            raise exc.End
+        if self.paths == None:
+            print("end")
         elif type(self.paths) is str:
-            self.next_area = self.paths
+            self.next_sector = self.paths
         # elif self.paths is None:
         #     raise exc.PathsImproperlyConfigured
 
     def display_available_path(self):
         """Convert underscore style to space for human read"""
-        
+
         paths = []
         for path in self.paths:
             paths.append(convert_to_readable(path))
@@ -58,8 +55,8 @@ class BaseArea:
         paths = self.path_separator.join(paths)
         print(f"\n{paths}")
 
-    def set_next_area(self):
-        """Set self.next_area for importing and executing"""
+    def set_next_sector(self):
+        """Set self.next_sector for importing and executing"""
 
         player_choice = input("> ")
 
@@ -70,16 +67,16 @@ class BaseArea:
         if chosen_path == False:
             print(f"Path {player_choice} doesn't exist")
         else:
-            self.next_area = chosen_path
+            self.next_sector = chosen_path
 
-    def import_next_area(self, next_area):
-        """Import the class of next area to instantiate in main.py"""
-        
-        return import_from_pack(f'area.{next_area}')
+    def import_next_sector(self, next_sector):
+        """Import the class of next sector to instantiate in main.py"""
+
+        return import_from_pack(f'sector.{next_sector}')
 
 
 class Dialogue:
-    """Class to be imported when an area need to display dialogue(s)"""
+    """Class to inherit when a sector need to display dialogue(s)"""
 
     dialogue = [
         {
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # from Mechanics import *
 from os import system
 from time import time
@@ -13,3 +14,42 @@ if __name__ == '__main__':
             player.location = getattr(Rooms, player.location)(player)
     except PlayerDead:
         Rooms.death(player)
+=======
+import sys
+
+from importlib import import_module
+from os import system
+
+from dungeonrun import config as cf
+from dungeonrun.actor.base import BaseActor
+
+
+if __name__ == '__main__':
+    try:
+        pack_config_path = 'pack.{pack_name}.config'.format(pack_name=cf.PACK_NAME)
+        pack_config = import_module(pack_config_path)
+        SECTOR_BEGIN_FILE, SECTOR_BEGIN_CLASS = pack_config.SECTOR_BEGIN.split('.')
+    except (AttributeError, ValueError):
+        print("""
+        Pack config error: Make sure 'SECTOR_BEGIN' in pack/{pack_name}/config.py is configured correctly
+        Example: 'FILE_NAME.CLASS_NAME'
+        """.format(pack_name=cf.PACK_NAME))
+        sys.exit(1)
+    except ModuleNotFoundError:
+        print("""
+        Pack error:
+        Make sure the pack is installed correctly and 'PACK_NAME' correctly configured in dungeonrun/config.py
+        PACK_NAME: {pack_name}
+        """.format(pack_name=cf.PACK_NAME))
+        sys.exit(1)
+
+    system('cls')
+    player = BaseActor()
+
+    sector_path = 'pack.{pack_name}.sector.{sector_begin}'.format(pack_name=cf.PACK_NAME, sector_begin=SECTOR_BEGIN_FILE)
+    sector = getattr(import_module(sector_path), SECTOR_BEGIN_CLASS)
+
+    while True:
+        system('cls')
+        sector = sector(player).execute()
+>>>>>>> df37bdb (refactor: renamed `area` module to `sector`)
