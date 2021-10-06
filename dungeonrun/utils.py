1166e0089e984209c7004a74b83a778bbf70e66f
Andrien Wiandyano <arnnied03@gmail.com>
2021-10-06 12:10:10 +0700

    feat(utils): added `timed()` function

--- dungeonrun/utils.py
+++ dungeonrun/utils.py
@@ -5,6 +5,7 @@ import inspect
 from random import random, uniform
 from importlib import import_module
 from inspect import getmro
+from time import sleep
 
 from dungeonrun import config
 
@@ -13,10 +14,23 @@ def rng() -> float:
 
     return random()
 
+def timed(text, before=0, after=0.75):
+    """
+    Add a pause before and/or after a text.
+
+    before: Mainly used for notification such as level up
+
+    after: Mainly used for dialogue
+    """
+
+    sleep(before)
+    print(text)
+    sleep(after)
+
 def convert_to_keys(string: str) -> str:
     """
     This will replace space in string with underscore for dictionary key use
-    
+
     'This Is An Example' -> 'this_is_an_example'
     """
 
@@ -26,7 +40,7 @@ def convert_to_readable(string: str) -> str:
     """
     This will replace underscore in string with space for user to read
 
-    'this_is_an_example' -> 'This Is An Example' 
+    'this_is_an_example' -> 'This Is An Example'
     """
 
     return ' '.join(string.split('_')).title()
@@ -35,14 +49,5 @@ def import_from_pack(path: str):
     """Handles importing module and class"""
 
     *file_directory, class_name = path.split('.')
-    
+
     return getattr(import_module(f"pack.{config.PACK_NAME}.{'.'.join(file_directory)}"), class_name)
-
-# def get_parents(class_):
-#     """
-#     Function to use when 'isinstance()' cannot be used.
-    
-#     Return name of all parent classes from 'class_'.
-#     """
-
-#     return [parent.__name__ for parent in getmro(class_)]
